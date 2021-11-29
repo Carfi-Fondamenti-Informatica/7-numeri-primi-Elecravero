7250d1280b31e2b0e0b2f24278dd04762ac2785d
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-11-29 11:15:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+
+    int numero=0, conta=0;
+    cin>>numero;
+
+    primo (numero, 1, conta);
+    if (conta==0){
+        cout<<"numero primo"<<endl;
+
+    } else {
+        cout<< "numero non primo"<<endl;
+    }
+    return 0;
 }
+
+
